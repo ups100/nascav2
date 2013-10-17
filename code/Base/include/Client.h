3e4889e2d99030e92b8a4a0ab245c227dfa99665
Krzysztof Opasiak <ups100@tlen.pl>
2013-10-17 23:36:44 +0200

    Add implementation of ClientSession class.
    Rename class Client to ClientSession which is
    more suitable. Add implementation and doxygen
    documentation.
    
    Signed-off-by: Krzysztof Opasiak <ups100@tlen.pl>

--- code/Base/include/Client.h
+++ code/Base/include/Client.h
@@ -1,6 +1,6 @@
 /**
  * @file  Client.h
- * @brief  Definition of the Class Client
+ * @brief  Definition of the Class ClientSession
  * @date  13-sie-2013 17:38:09
  * @author Krysztof Opasiak <ups100@tlen.pl>
  */
@@ -9,24 +9,114 @@
 #define EA_D6440D50_CCA5_46f1_8A41_6416AF2C70A2__INCLUDED_
 
 #include <QString>
+#include "AAAModule.h"
+#include "AAAFactory.h"
+#include "ConversationInterface.h"
+#include <boost/shared_ptr.hpp>
 
 namespace INZ_project {
 namespace Base {
 
 class DataChannel;
+class DataProvider;
 
-class Client
+/**
+ * @brief Class which represents a single client connection.
+ */
+class ClientSession
 {
 
 public:
-    Client();
-    virtual ~Client();
+    /**
+     * @brief Constructor
+     * @param[in] clientId of client
+     * @param[in] provider to which client has connected
+     * @throws ClientException if no such client
+     */
+    ClientSession(const QString& clientId, const DataProvider* provider);
 
-    const QString& getClientId();
-    const DataChannel& getDataChannel();
+    /**
+     * @brief Destructor
+     */
+    virtual ~ClientSession();
 
+    /**
+     * @brief Gets the client id
+     * @return Client ID
+     */
+    const QString& getClientId() const;
+
+    /**
+     * @brief Gets the list of available AAA modules for this client
+     * @return List of available modules
+     * @throws ClientException if no modules available for client
+     */
+    const QList<QString>& getAAAModulesList();
+
+    /**
+     * @brief Authorize a client using a given AAAModule and conversation interface
+     * @param[in] aaaModuleId id of module to use for authorization
+     * @param[in] interface for conversation with client. This function does not
+     * take the ownership of this object. You should delete it after return from this
+     * function.
+     * @return true if client has been successfully authorized with this module,
+     * false otherwise
+     * @throws ClientException if given module is unavailable for this client
+     */
+    bool authorize(const QString& aaaModuleId,
+            AAA::ConversationInterface* interface);
+
+    /**
+     * @brief Check whether this client has been successfully authorized
+     * @return true if client has been authorized, false otherwise
+     */
+    bool isAuthorized();
+
+    /**
+     * @brief Gets the data channel for this client
+     * @return Data channel associated with this client
+     * @note This object has an ownership of returned object so you should
+     * not delete it.
+     * @throws ClientException if client has not been authorized
+     */
+    DataChannel* getDataChannel();
+
+    /**
+     * @brief Exception class for client class
+     */
+    class ClientException : public std::runtime_error
+    {
+    public:
+        /**
+         * @brief Constructor
+         * @param[in] arg exception string
+         */
+        explicit ClientException(const std::string& arg)
+                : std::runtime_error(arg)
+        {
+        }
+    };
 private:
-    DataChannel *m_DataChannel;
+    /**
+     * @brief Id of client
+     */
+    QString m_clientId;
+    /**
+     * @brief Provider id
+     */
+    QString m_providerId;
+    /**
+     * @brief List of AAAModules accessible for this client
+     */
+    QList<QString> m_aaaModules;
+    /**
+     * @brief Information if client has been already authorized
+     */
+    bool m_isAuthorized;
+    /**
+     * @brief Data channel to save received data.
+     */
+    boost::shared_ptr<DataChannel> m_dataChannel;
 };
 
 } //namespace Base
--- code/Base/src/Client.cpp
+++ code/Base/src/Client.cpp
@@ -1,36 +1,122 @@
 /**
  * @file  Client.h
- * @brief  Implementation of the Class Client
- * @date  Created on:      13-sie-2013 17:38:09
+ * @brief  Implementation of the Class ClientSession
+ * @date  17-10-2013 17:38:09
  * @author Krysztof Opasiak <ups100@tlen.pl>
  */
 
 #include "Client.h"
 #include "DataChannel.h"
+#include "LogEntry.h"
+#include "DataProvider.h"
+#include "ConfigurationParser.h"
+#include "MyNscaMain.h"
 
 namespace INZ_project {
 namespace Base {
 
-Client::Client()
+ClientSession::ClientSession(const QString& clientId,
+        const DataProvider* provider)
+        : m_clientId(clientId), m_providerId(provider->getProviderId()),
+                m_isAuthorized(false), m_dataChannel(static_cast<DataChannel*>(0L))
+{
+    try {
+        m_aaaModules = ConfigurationParser::getAAAModulesForClient(clientId);
+    } catch (const ConfigurationParser::ParserException& e) {
+        LOG_ENTRY(MyLogger::DEBUG, e.what()<<" While id = "<<clientId);
+        throw ClientException(e.what());
+    }
+}
+
+ClientSession::~ClientSession()
 {
 
 }
 
-Client::~Client()
+const QList<QString>& ClientSession::getAAAModulesList()
 {
+    if (m_aaaModules.isEmpty()) {
+        throw ClientException("No AAAModules allowed for this client");
+    }
 
+    return m_aaaModules;
 }
 
-const QString& Client::getClientId()
+bool ClientSession::authorize(const QString& aaaModuleId,
+        AAA::ConversationInterface* interface)
 {
+    if (!m_aaaModules.contains(aaaModuleId)) {
+        LOG_ENTRY(MyLogger::DEBUG,
+                "Trying to authorize client "<<m_clientId <<" using unsupported AAAModule: "<<aaaModuleId);
+        throw ClientException("AAAModule not allowed for this client");
+    }
 
-    return QString();
+    boost::shared_ptr<AAA::AAAModule> module = boost::shared_ptr<AAA::AAAModule>(AAA::AAAFactory::getAAAModule(
+            aaaModuleId));
+
+    if (module == 0L) {
+        LOG_ENTRY(MyLogger::DEBUG,
+                "Trying to authorize client "<<m_clientId <<" using unavailable AAAModule: "<<aaaModuleId);
+        throw ClientException("AAAModule unavailable");
+    }
+
+    //Let's set the method for conversation
+    module->setConversationInterface(interface);
+
+    try {
+        //set the additional data about this client
+        module->setAdditionalData(
+                ConfigurationParser::getAAAModuleData(m_clientId, aaaModuleId));
+        //and let's try to authorize
+        m_isAuthorized = module->run();
+
+        //if authorization is successful client will send some data so let's
+        //provide data channel for this client
+        if (m_isAuthorized) {
+            m_dataChannel = boost::shared_ptr<DataChannel>(MyNscaMain::provideDataChannel(m_clientId,
+                    m_providerId));
+        }
+    } catch (const ConfigurationParser::ParserException& e) {
+        LOG_ENTRY(MyLogger::FATAL,
+                "Unable to get additional data of "<<aaaModuleId<<"for "<<m_clientId);
+        throw ClientException("Unable to get additional data.");
+    } catch (const AAA::AAAModule::AdditionalDataException& e) {
+        LOG_ENTRY(MyLogger::FATAL,
+                "Unable to parse data for "<<aaaModuleId<<" client: "<<m_clientId);
+        throw ClientException("Unable to parse additional data.");
+    } catch (const std::runtime_error& e) {
+        //some error while trying to authorize client
+        LOG_ENTRY(MyLogger::DEBUG,
+                e.what()<< "While trying to authorize using "<<aaaModuleId<<" client: "<<m_clientId);
+        m_isAuthorized = false;
+    } catch (...) {
+        //unknown error while trying to authorize client
+        LOG_ENTRY(MyLogger::DEBUG,
+                "Unknown error while trying to authorize using "<<aaaModuleId<<" client: "<<m_clientId);
+        m_isAuthorized = false;
+    }
+
+    return m_isAuthorized;
 }
 
-const DataChannel& Client::getDataChannel()
+bool ClientSession::isAuthorized()
+{
+    return m_isAuthorized;
+}
+
+const QString& ClientSession::getClientId() const
 {
 
-    return DataChannel();
+    return m_clientId;
+}
+
+DataChannel* ClientSession::getDataChannel()
+{
+    if(!m_isAuthorized) {
+        throw ClientException("Client not authorized");
+    }
+
+    return m_dataChannel.get();
 }
 
 } //namespace Base
