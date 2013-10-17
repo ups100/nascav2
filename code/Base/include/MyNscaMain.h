1325d18ccc5bf65abf954bdaf345384f1ca8488b
Krzysztof Opasiak <ups100@tlen.pl>
2013-10-17 23:46:53 +0200

    Add implementation of main program class.
    Add partial implementation of main program class.
    
    Signed-off-by: Krzysztof Opasiak <ups100@tlen.pl>

---
+++ code/Base/include/MyNscaMain.h
@@ -0,0 +1,221 @@
+/**
+ * @file  MyNscaMain.h
+ * @brief  Definition of the Class MyNascaMain
+ * @date  13-09-2013 17:38:12
+ * @author Krysztof Opasiak <ups100@tlen.pl>
+ */
+
+#if !defined(EA_D5A037F0_18C8_4dda_9955_7BDA9D7E33F3__INCLUDED_)
+#define EA_D5A037F0_18C8_4dda_9955_7BDA9D7E33F3__INCLUDED_
+
+#include "DataProvider.h"
+#include "DataConsumer.h"
+#include "ConfigurationParser.h"
+#include <QMap>
+#include <QString>
+#include <boost/shared_ptr.hpp>
+#include <QCoreApplication>
+
+namespace INZ_project {
+namespace Base {
+
+/**
+ * @brief Main class for whole program
+ */
+class MyNscaMain : public QObject
+{
+Q_OBJECT
+    ;
+public:
+    /**
+     * @brief Constructor
+     * @param[in] argc argument counter
+     * @param[in] argv argument value
+     */
+    MyNscaMain(int argc, char **argv);
+
+    /**
+     * @brief Destructor
+     * @details This object should not be destroyed while being in exec() function
+     */
+    virtual ~MyNscaMain();
+
+    /**
+     * @brief Makes command line argument parsing, creates all necessary objects
+     * and starts the program main loop
+     */
+    int exec();
+
+    /**
+     * @brief Provides data channel for specific client and data provider
+     * @details Use this function to obtain a convenient way to provide received logs
+     * to correct data consumers
+     * @param[in] client which has been connected
+     * @param[in] provider to which client has been connected
+     * @return object which should be used as communication channel
+     */
+    static DataChannel* provideDataChannel(const QString& client,
+            const QString& provider);
+
+    /**
+     * @todo implement me
+     * @brief Shuts down the whole program
+     */
+    static void shutDown();
+
+    /**
+     * @brief Gets the consumer with given id
+     * @param[in] id of consumer to obtain
+     * @return Data consumer associated with this id or NULL if no such consumer
+     */
+    static DataConsumer* getConsumer(const QString& id);
+
+    /**
+     * @brief Exception class for errors occurred while parsing
+     */
+    class MyNscaMainException : public std::runtime_error
+    {
+    public:
+        /**
+         * @brief Constructor
+         * @param[in] arg exception string
+         */
+        explicit MyNscaMainException(const std::string& arg)
+                : std::runtime_error(arg)
+        {
+        }
+    };
+private slots:
+
+    /**
+     * @brief Slot used to initialize data consumer
+     */
+    void initDataConsumers();
+
+    /**
+     * @brief Slot used to initialize data Providers
+     */
+    void initDataProviders();
+
+signals:
+
+    /**
+     * @brief Signal emitted when all data consumers have been successfully initialized.
+     */
+    void dataConsummersReady();
+
+    /**
+     * @brief Signal emitted when something goes wrong while initialization and
+     * we have to shut down the program
+     */
+    void emergencyShutdown();
+
+    /**
+     * @brief Signal emitted when all data providers have been successfully initialized.
+     */
+    void dataProvidersReady();
+
+private:
+
+    /**
+     * @brief Global instance of this class.
+     */
+    static MyNscaMain *m_instance;
+
+    /**
+     * @brief Get's the instance of this class
+     */
+    static MyNscaMain* getInstance();
+
+    /**
+     * @brief Enumeration with possible actions.
+     */
+    enum WhatToDo
+    {
+        START_PROGRAM,
+        PRINT_HELP,
+        CHECK_CONFIGURATION_FILE,
+        START_ONLY_CONSUMERS
+    };
+
+    /**
+     * @todo Implement me
+     * @brief Parses the command line arguments and told what to do
+     * @return Action requested by user
+     */
+    WhatToDo parseCommandLineArgs();
+
+    /**
+     * @brief Starts the program
+     * @return 0 if program ends without any error, non zero value otherwise
+     */
+    int startProgram();
+
+    /**
+     * @todo Implement me
+     * @brief Prints the help text and exits
+     * @return 0 if program ends without any error, non zero value otherwise
+     */
+    int printHelp();
+
+    /**
+     * @brief Checks the configuration file and exits
+     * @return 0 if program ends without any error, non zero value otherwise
+     */
+    int checkConfigurationFile();
+
+    /**
+     * @brief Starts only the buffer files and data consumers.
+     * @return 0 if program ends without any error, non zero value otherwise
+     */
+    int startOnlyConsumers();
+
+    /**
+     * @brief Creates data consumers according to configuration file.
+     */
+    int createDataConsumers();
+
+    /**
+     * @brief Creates data providers according to configuration file.
+     */
+    int createDataProviders();
+
+    /**
+     * @brief Creates data files for data providers according to configuration file.
+     */
+    int createDataFiles();
+
+    /**
+     * @brief Active data providers
+     */
+    QMap<QString, boost::shared_ptr<DataProvider> > m_dataProviders;
+    /**
+     * @brief Active data consumers
+     */
+    QMap<QString, boost::shared_ptr<DataConsumer> > m_dataConsumers;
+    /**
+     * @brief Active data consumers
+     */
+    QMap<QString, boost::shared_ptr<DataFile> > m_dataFiles;
+    /**
+     * @brief Main event loop
+     */
+    QCoreApplication m_app;
+    /**
+     * @brief Path to configuration file
+     */
+    QString m_confPath;
+    /**
+     * @brief Directory where buffer files should be placed
+     */
+    QString m_bufPath;
+
+    /**
+     * @brief Maximum size of each buffer file.
+     */
+    qint64 m_maxBufSize;
+};
+
+} //namespace Base
+} //namespace INZ_project
+#endif // !defined(EA_D5A037F0_18C8_4dda_9955_7BDA9D7E33F3__INCLUDED_)
---
+++ code/Base/src/MyNscaMain.cpp
@@ -0,0 +1,349 @@
+/**
+ * @file  MyNscaMain.h
+ * @brief  Implementation of the Class MyNascaMain
+ * @date  20-09-2013 17:38:12
+ * @author Krysztof Opasiak <ups100@tlen.pl>
+ */
+
+#include "MyNscaMain.h"
+#include "LogEntry.h"
+#include "DataConsumerFactory.h"
+#include "DataProviderFactory.h"
+#include "DataFile.h"
+#include <QFile>
+#include <QTimer>
+
+namespace INZ_project {
+namespace Base {
+
+MyNscaMain* MyNscaMain::m_instance = 0L;
+
+MyNscaMain::MyNscaMain(int argc, char **argv)
+        : m_app(argc, argv)
+{
+    if (m_instance == 0L) {
+        m_instance = this;
+    } else {
+        LOG_ENTRY(MyLogger::FATAL,
+                "Another instance of MyNscaMain has been created!");
+        throw MyNscaMainException("Another instance is alive");
+    }
+}
+
+MyNscaMain::~MyNscaMain()
+{
+    if (m_instance == this) {
+        m_instance = 0L;
+    } else {
+        LOG_ENTRY(MyLogger::FATAL, "Instance overwritten!!!");
+    }
+}
+
+MyNscaMain* MyNscaMain::getInstance()
+{
+    if (m_instance == 0L) {
+        throw MyNscaMainException("No instance created!");
+    }
+
+    return m_instance;
+}
+
+int MyNscaMain::exec()
+{
+    int ret = 0;
+    switch (parseCommandLineArgs()) {
+        case START_PROGRAM:
+            ret = startProgram();
+            break;
+        case PRINT_HELP:
+            ret = printHelp();
+            break;
+        case CHECK_CONFIGURATION_FILE:
+            ret = checkConfigurationFile();
+            break;
+        case START_ONLY_CONSUMERS:
+            ret = startOnlyConsumers();
+            break;
+    }
+
+    return ret;
+}
+
+int MyNscaMain::startOnlyConsumers()
+{
+    int ret = 0;
+    if ((ret = checkConfigurationFile()) != 0) {
+        return ret;
+    }
+
+    //create data consumers
+    if ((ret = createDataConsumers()) != 0) {
+        return ret;
+    }
+
+    //set the buffer files
+    if ((ret = createDataFiles()) != 0) {
+        return ret;
+    }
+
+    //schedule initialization slot to be run
+    QTimer::singleShot(0, this, SLOT(initDataConsumers()));
+
+    //run the main loop.
+    return m_app.exec();
+}
+
+MyNscaMain::WhatToDo MyNscaMain::parseCommandLineArgs()
+{
+    return START_PROGRAM;
+}
+
+int MyNscaMain::startProgram()
+{
+    int ret = 0;
+    if ((ret = checkConfigurationFile()) != 0) {
+        return ret;
+    }
+
+    //create data consumers
+    if ((ret = createDataConsumers()) != 0) {
+        return ret;
+    }
+
+    //create data providers
+    if ((ret = createDataProviders()) != 0) {
+        return ret;
+    }
+
+    //set the buffer files
+    if ((ret = createDataFiles()) != 0) {
+        return ret;
+    }
+
+    //This is a full start so connect all signals
+    connect(this, SIGNAL(dataConsummersReady()), this,
+            SLOT(initDataProviders()));
+
+    //schedule initialization slot to be run
+    QTimer::singleShot(0, this, SLOT(initDataConsumers()));
+
+    //run the main loop.
+    return m_app.exec();
+}
+
+int MyNscaMain::printHelp()
+{
+    LOG_ENTRY(MyLogger::FATAL, "Help text");
+    return -1;
+}
+
+int MyNscaMain::checkConfigurationFile()
+{
+    QFile config(m_confPath);
+    if (!config.exists()) {
+        LOG_ENTRY(MyLogger::FATAL, "No such file: "<<m_confPath);
+        return -1;
+    } else {
+        LOG_ENTRY(MyLogger::DEBUG, "File "<<m_confPath<<" exist, opening..");
+    }
+
+    if (!config.open(QIODevice::ReadOnly)) {
+        LOG_ENTRY(MyLogger::FATAL,
+                "Unable to open configuration file: "<<m_confPath);
+        return -1;
+    } else {
+        LOG_ENTRY(MyLogger::DEBUG,
+                "File "<<m_confPath<<" successfully opened for reading.");
+    }
+
+    bool result = ConfigurationParser::parseConfiguration(&config);
+    config.close();
+
+    if (result) {
+        LOG_ENTRY(MyLogger::DEBUG, "Configuration file OK.");
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+DataChannel* MyNscaMain::provideDataChannel(const QString& client,
+        const QString& provider)
+{
+    DataChannel* channel = 0L;
+    try {
+        MyNscaMain *instance = MyNscaMain::getInstance();
+        if (client != 0L && provider != 0L
+                && instance->m_dataFiles.contains(provider)) {
+            try {
+                channel = new DataChannel(instance->m_dataFiles[provider].get(),
+                        ConfigurationParser::getRoutes(client, provider),
+                        client, provider);
+            } catch (const DataChannel::BadIdException &e) {
+                LOG_ENTRY(MyLogger::ERROR,
+                        "Cannot create data channel for: " <<client<<" connected to "<<provider);
+                //only to clarify
+                channel = 0L;
+            }
+        }
+
+    } catch (const MyNscaMainException& e) {
+        LOG_ENTRY(MyLogger::FATAL, e.what());
+    }
+
+    return channel;
+}
+
+DataConsumer* MyNscaMain::getConsumer(const QString& id)
+{
+
+    DataConsumer *consumer = 0L;
+    try {
+        MyNscaMain *instance = MyNscaMain::getInstance();
+
+        if (instance->m_dataConsumers.contains(id)) {
+            consumer = instance->m_dataConsumers[id].get();
+        }
+    } catch (const MyNscaMainException& e) {
+        LOG_ENTRY(MyLogger::FATAL, e.what());
+    }
+
+    return consumer;
+}
+
+void MyNscaMain::initDataConsumers()
+{
+    QList<QString> consumers = m_dataConsumers.keys();
+    QString errorWhileConsumer;
+
+    foreach(QString consumer, consumers) {
+        if (m_dataConsumers[consumer]->init(
+                ConfigurationParser::getDataConsumerAdditionalData(consumer),
+                consumer, ConfigurationParser::getDataConsumerType(consumer))
+                != 0) {
+            //we have some error while initialization of data consumer
+            LOG_ENTRY(MyLogger::FATAL,
+                    "Error while initialization of "<<consumer);
+            errorWhileConsumer = consumer;
+            break;
+        }
+    }
+
+    if (errorWhileConsumer != QString()) {
+        //there was some error while initialization of consumers,
+        //we have to stop those who are started
+        foreach(QString consumer, consumers) {
+            if (consumer != errorWhileConsumer) {
+                m_dataConsumers[consumer]->close();
+            } else {
+                m_dataConsumers.clear();
+                emit emergencyShutdown();
+                break;
+            }
+        }
+    } else {
+        emit dataConsummersReady();
+    }
+}
+
+void MyNscaMain::initDataProviders()
+{
+    QList<QString> providers = m_dataProviders.keys();
+    QString errorWhileProvider;
+
+    foreach(QString provider, providers) {
+        if (m_dataProviders[provider]->init(
+                ConfigurationParser::getDataProviderAdditionalData(provider),
+                provider, ConfigurationParser::getDataProviderType(provider))
+                != 0) {
+            //we have some error while initialization of data consumer
+            LOG_ENTRY(MyLogger::FATAL,
+                    "Error while initialization of "<<provider);
+            errorWhileProvider = provider;
+            break;
+        }
+    }
+
+    if (errorWhileProvider != QString()) {
+        //there was some error while initialization of providers,
+        //we have to stop those who are started
+        foreach(QString provider, providers) {
+            if (provider != errorWhileProvider) {
+                m_dataProviders[provider]->close();
+            } else {
+                m_dataProviders.clear();
+                emit emergencyShutdown();
+                break;
+            }
+        }
+    } else {
+        emit dataProvidersReady();
+    }
+}
+
+int MyNscaMain::createDataConsumers()
+{
+    QSet<QString> consumers = ConfigurationParser::getDataConsumersNames();
+    foreach(QString consumer, consumers) {
+        QString type;
+        try {
+            type = ConfigurationParser::getDataConsumerType(consumer);
+        } catch (std::runtime_error &e) {
+            LOG_ENTRY(MyLogger::FATAL, e.what()<<" in "<<consumer);
+            return -1;
+        }
+        boost::shared_ptr<DataConsumer> cons(
+                DataConsumerFactory::getDataConsumer(type));
+        if (cons.get() == 0L) {
+            LOG_ENTRY(MyLogger::FATAL,
+                    "No such type: "<<type<<" in consumer factory");
+            return -1;
+        }
+        m_dataConsumers[consumer] = cons;
+    }
+
+    return 0;
+}
+
+int MyNscaMain::createDataProviders()
+{
+    QSet<QString> providers = ConfigurationParser::getDataProvidersNames();
+    foreach(QString provider, providers) {
+        QString type;
+        try {
+            type = ConfigurationParser::getDataProviderType(provider);
+        } catch (std::runtime_error &e) {
+            LOG_ENTRY(MyLogger::FATAL, e.what()<<" in "<<provider);
+            return -1;
+        }
+        boost::shared_ptr<DataProvider> prov(
+                DataProviderFactory::getDataProvider(type));
+        if (prov.get() == 0L) {
+            LOG_ENTRY(MyLogger::FATAL,
+                    "No such type: "<<type<<" in consumer factory");
+            return -1;
+        }
+        m_dataProviders[provider] = prov;
+    }
+
+    return 0;
+}
+
+int MyNscaMain::createDataFiles()
+{
+    QSet<QString> providers = ConfigurationParser::getDataProvidersNames();
+    foreach(QString provider, providers) {
+        m_dataFiles[provider] = boost::shared_ptr<DataFile>(new DataFile());
+        if (!m_dataFiles[provider]->setFile(m_bufPath + "/" + provider,
+                m_maxBufSize)) {
+            LOG_ENTRY(MyLogger::FATAL,
+                    "Unable to use "<<m_bufPath + "/" + provider <<" as a buffer file.");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+} //namespace Base
+} //namespace INZ_project
