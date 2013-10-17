4fc3b8c74a8cc35d5d436d506fa3056ad35488ba
Krzysztof Opasiak <ups100@tlen.pl>
2013-10-17 23:28:08 +0200

    Add implementation of DataChannel class.
    Add implementation of mentioned class
    and adjust it to new convetion with temporary
    logs storing.
    
    Signed-off-by: Krzysztof Opasiak <ups100@tlen.pl>

--- code/Base/include/DataChannel.h
+++ code/Base/include/DataChannel.h
@@ -1,22 +1,126 @@
 /**
  * @file  DataChannel.h
  * @brief  Definition of the Class DataChannel
- * @date  13-sie-2013 17:38:11
+ * @date  13-09-2013 17:38:11
  * @author Krysztof Opasiak <ups100@tlen.pl>
  */
 
 #if !defined(EA_ECD19921_6648_4524_87B9_6086B11E331C__INCLUDED_)
 #define EA_ECD19921_6648_4524_87B9_6086B11E331C__INCLUDED_
 
+#include <QString>
+#include <QSet>
+#include <QMap>
+#include <stdexcept>
+
 namespace INZ_project {
 namespace Base {
+
+class DataPortion;
+class DataFile;
+class DataConsumer;
+
+/**
+ * @brief Channel for passing logs to all required data consumers
+ */
 class DataChannel
 {
 
 public:
-    DataChannel();
+    /**
+     * @brief Constructor
+     * @details Creates a dataChannel with file as a buffer and
+     * with specific destinations
+     *
+     * @param[in] file to be used as a buffer, This object do not take
+     * the ownership of this parameter
+     *
+     * @param[in] destinations places where logs will be provided via
+     * this channel. This Set should contain valid DataConsumers ID's
+     *
+     * @param[in] client which use this channel
+     * @param[in] provider which is connected with this client
+     *
+     * @throws BadDestinationException if one or more destinations does not exist
+     */
+    DataChannel(DataFile *file, const QSet<QString>& destinations,
+            const QString& client, const QString& provider);
+
+    /**
+     * @brief Destructor
+     */
     virtual ~DataChannel();
 
+    /**
+     * @brief Writes a data portion to a buffer.
+     * @details When this function returns successfully you may be sure
+     * that passed logs are persistently stored and will be delivered to all
+     * required places.
+     *
+     * @param[in] portion of logs to be stored
+     * @return true on success, false if unable to submit this log portion
+     * for example if there is currently not enough space in buffer.
+     */
+    bool write(const DataPortion& portion);
+
+    /**
+     * @brief Get's the size of free space required to store this data portion
+     * @param[in] portion of logs
+     * @return Size of required free space to store this logs portion
+     */
+    qint64 getSize(const DataPortion& portion) const;
+
+    /**
+     * @brief Get's the maximum size of buffer
+     * @return Maximum buffer size
+     */
+    qint64 getBufferSize() const;
+
+    /**
+     * @brief Gets the current free space size
+     * @return current free space size.
+     */
+    qint64 getFreeSpaceSize() const;
+
+    /**
+     * @brief Exception thrown then invalid data has been provided
+     */
+    class BadIdException : public std::runtime_error
+    {
+    public:
+        /**
+         * @brief Constructor
+         * @param[in] arg exception string
+         */
+        explicit BadIdException(const std::string& arg)
+                : std::runtime_error(arg)
+        {
+        }
+    };
+
+private:
+    /**
+     * @brief Buffer where logs are stored
+     */
+    DataFile *m_file;
+    /**
+     * @brief Consumers which should receive logs via this data channel
+     */
+    QMap<QString, DataConsumer*> m_consumers;
+    /**
+     * @brief Client which sends logs via this data channel
+     */
+    QString m_client;
+
+    /**
+     * @brief Provider which pass this logs to channel
+     */
+    QString m_provider;
+
+    /**
+     * @brief Ready to store logs header
+     */
+    QByteArray m_header;
 };
 
 } //namespace Base
--- code/Base/src/DataChannel.cpp
+++ code/Base/src/DataChannel.cpp
@@ -1,23 +1,97 @@
 /**
- * @file  DataChannel.h
- * @brief  Implementation of the Class DataChannel
- * @date  Created on:      13-sie-2013 17:38:11
+ * @file  DataChannel.cpp
+ * @brief Implementation of the Class DataChannel
+ * @date  13-09-2013 17:38:11
  * @author Krysztof Opasiak <ups100@tlen.pl>
  */
 
 #include "DataChannel.h"
+#include "DataPortion.h"
+#include "DataFile.h"
+#include "MyNscaMain.h"
+#include "LogEntry.h"
+#include <QObject>
 
 namespace INZ_project {
 namespace Base {
 
-DataChannel::DataChannel()
+DataChannel::DataChannel(DataFile *file, const QSet<QString>& destinations,
+        const QString& client, const QString& provider)
+        : m_file(file), m_client(client), m_provider(provider)
 {
+    foreach(QString dst, destinations) {
+        DataConsumer *cons = MyNscaMain::getConsumer(dst);
+        if (cons == 0L) {
+            LOG_ENTRY(MyLogger::ERROR, "No such data consumer: "<<dst);
+            throw BadIdException(dst.toStdString());
+        } else {
+            m_consumers[dst] = cons;
+        }
+    }
+
+    if (m_client.at(m_client.size() - 1) != '\n') {
+        m_client += "\n";
+    }
+
+    if (m_provider.at(m_provider.size() - 1) != '\n') {
+        m_provider += "\n";
+    }
+
+    //Header is const so let's create one and reuse it
+    QTextStream stream(&m_header);
+    stream << m_client << m_provider << m_consumers.size() << '\n';
+    foreach(QString dest, destinations) {
+        stream << dest;
+        if (dest.at(dest.size() - 1) != '\n') {
+            stream << '\n';
+        }
+    }
 
 }
 
 DataChannel::~DataChannel()
 {
+    m_consumers.clear();
+}
 
+bool DataChannel::write(const DataPortion& portion)
+{
+    bool ret = true;
+    if (portion.getClient() != m_client) {
+        LOG_ENTRY(MyLogger::DEBUG,
+                "Trying to write logs from: "<<portion.getClient()
+                <<"to channel which belongs to "<<m_client);
+        ret = false;
+    }
+
+    if (portion.getProvider() != m_provider) {
+        LOG_ENTRY(MyLogger::DEBUG,
+                "Trying to write logs received via: "<<portion.getProvider()
+                <<"to channel which starts in "<<m_provider);
+        ret = false;
+    }
+
+    if (ret) {
+        ret = m_file->write(m_client, m_provider, m_consumers, m_header,
+                portion.getIcingaFormated());
+    }
+
+    return ret;
+}
+
+qint64 DataChannel::getSize(const DataPortion& portion) const
+{
+    return m_header.size() + portion.getSize();
+}
+
+qint64 DataChannel::getBufferSize() const
+{
+    return m_file->getMaxFileSize();
+}
+
+qint64 DataChannel::getFreeSpaceSize() const
+{
+    return m_file->getFreeSpaceSize();
 }
 
 } //namespace Base
