3f271ec5cb4e892ac18d74674ed9832703b36a8b
Krzysztof Opasiak <ups100@tlen.pl>
2013-11-28 23:01:28 +0100

    Add test authorization module.
    Add authorization module which always allow
    user to log in for test purpouse.
    
    Signed-off-by: Krzysztof Opasiak <ups100@tlen.pl>

---
+++ code/AAA/include/AlwaysAllow.h
@@ -0,0 +1,28 @@
+/**
+ * @file  AlwaysAllow.h
+ * @brief  Definition of the Class INZ_project::AAA::AlwaysAllow
+ * @date  28-11-2013 18:18:09
+ * @author Krysztof Opasiak <ups100@tlen.pl>
+ */
+
+#ifndef __ALWAYS_ALLOW_H__
+#define __ALWAYS_ALLOW_H__
+
+#include "AAAModuleRegister.h"
+
+namespace INZ_project {
+namespace AAA {
+
+AAAMODULE(AlwaysAllow, AlwaysAllow)
+{
+public:
+    AlwaysAllow();
+    virtual ~AlwaysAllow();
+    virtual AAAModule* clone();
+    virtual void setAdditionalData(const QString &additionalData);
+    virtual bool run();
+};
+
+} //namespace AAA
+} //namespace INZ_project
+#endif // __ALWAYS_ALLOW_H__
---
+++ code/AAA/src/AlwaysAllow.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file  AlwaysAllow.cpp
+ * @brief  Definition of the Class INZ_project::AAA::AlwaysAllow
+ * @date  28-11-2013 18:18:09
+ * @author Krysztof Opasiak <ups100@tlen.pl>
+ */
+
+#include "AlwaysAllow.h"
+
+namespace INZ_project {
+namespace AAA {
+
+AlwaysAllow::AlwaysAllow()
+{
+
+}
+
+AlwaysAllow::~AlwaysAllow()
+{
+
+}
+
+AAAModule* AlwaysAllow::clone()
+{
+    return new AlwaysAllow();
+}
+
+void AlwaysAllow::setAdditionalData(const QString &additionalData)
+{
+
+}
+
+bool AlwaysAllow::run()
+{
+    return true;
+}
+
+} //namespace AAA
+} //namespace INZ_project
