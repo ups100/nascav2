69fb31c45cbae4ba2ee3159b8169081128a322e3
Krzysztof Opasiak <ups100@tlen.pl>
2013-12-01 17:58:00 +0100

    Implementation of DefaultTcpProvider.
    Implement of mentioned class so it now starts
    tcp server in additional thread.
    
    Signed-off-by: Krzysztof Opasiak <ups100@tlen.pl>

--- code/Base/include/DefaultTcpProvider.h
+++ code/Base/include/DefaultTcpProvider.h
@@ -6,30 +6,63 @@
  */
 
 #include <QThread>
+#include <QHostAddress>
 #include "AdditionalThread.h"
 #include "DataProviderRegister.h"
 
 namespace INZ_project {
 namespace Base {
 
-DATA_PROVIDER(DefaultTcpProvider, DefaultTcpProvider)
+/**
+ * @brief Base data provider which use TCP.
+ * @details This class starts the new thread.
+ */DATA_PROVIDER(DefaultTcpProvider, DefaultTcpProvider)
 {
 public:
+    /**
+     * @brief Constructor
+     */
     DefaultTcpProvider();
 
+    /**
+     * @brief Destructor
+     */
     virtual ~DefaultTcpProvider();
 
     virtual void close();
 
 protected:
-    virtual int initImpl(const QString& additionalData, const QString& providerId,
-                const QString& providerType);
+    virtual int initImpl(const QString& additionalData,
+            const QString& providerId, const QString& providerType);
 
 private:
 
+    /**
+     * @brief Parses the #options and search for values like ip or port
+     * @details The format should be IP: <IP_ADDR> \n and Port: <port> \n
+     * @param[in] options string from which options should be parsed
+     */
+    int parseOptions(const QString& options);
+
+    /**
+     * @brief Main function of the additional thread
+     */
     int run();
 
+    /**
+     * @brief Additional thread for logic of this class
+     */
     TcpStandardModule::AdditionalThread m_thread;
+
+    /**
+     * @brief Address which should be used to listen for incoming connections
+     */
+    QHostAddress m_address;
+
+    /**
+     * @brief Address which should be used to listen for incoming connections
+     */
+    qint16 m_port;
 };
 
 } //namespace Base
--- code/Base/src/DefaultTcpProvider.cpp
+++ code/Base/src/DefaultTcpProvider.cpp
@@ -7,13 +7,17 @@
 
 #include "DefaultTcpProvider.h"
 #include "LogEntry.h"
+#include "TcpServer.h"
 #include <boost/bind.hpp>
+
+using namespace INZ_project::TcpStandardModule;
 
 namespace INZ_project {
 namespace Base {
 
 DefaultTcpProvider::DefaultTcpProvider()
-        : m_thread(boost::bind(&DefaultTcpProvider::run, this))
+        : m_thread(boost::bind(&DefaultTcpProvider::run, this)),
+                m_address(QHostAddress::Any), m_port(0)
 {
 
 }
@@ -31,14 +35,80 @@ void DefaultTcpProvider::close()
 int DefaultTcpProvider::initImpl(const QString& additionalData,
         const QString& providerId, const QString& providerType)
 {
+    if (parseOptions(additionalData) != 0) {
+        LOG_ENTRY(MyLogger::FATAL, "Unable to parse given options.");
+        return -1;
+    }
     m_thread.startThread();
     return 0;
 }
 
 int DefaultTcpProvider::run()
 {
-    LOG_ENTRY(MyLogger::INFO, "Zajebiaszczo");
+    TcpServer server;
+    if (!server.listen(m_address, m_port)) {
+        LOG_ENTRY(MyLogger::FATAL,
+                "Unable to listen at "<<m_address.toString()<<":"<<m_port);
+        ///////////////////////////
+        // TODO add error handling
+        //////////////////////////
+        return -1;
+    } else {
+        LOG_ENTRY(MyLogger::INFO,
+                "Listening at "<<m_address.toString()<<":"<<server.serverPort())
+    }
     return m_thread.exec();
+}
+
+int DefaultTcpProvider::parseOptions(const QString& options)
+{
+    static const QString ipString = "IP:";
+    static const QString portString = "Port:";
+
+    int pos = -1, nlPos = -1;
+
+    //check if there is IP to set
+    if ((pos = options.indexOf(ipString)) != -1) {
+        nlPos = options.indexOf("\n", pos);
+        QString address = options.mid(pos + ipString.size(),
+                nlPos - pos - ipString.size());
+
+        if (!m_address.setAddress(address)) {
+            LOG_ENTRY(MyLogger::ERROR,
+                    "Value: "<<address<<" is not a valid IP address.");
+            return -1;
+        } else {
+            LOG_ENTRY(MyLogger::INFO, "IP address provided: "<<address);
+        }
+    } else {
+        LOG_ENTRY(MyLogger::INFO,
+                "IP address has not been provided. Default: "<<m_address.toString()<<" used.");
+
+    }
+
+    //check if there is port to set
+    if ((pos = options.indexOf(portString)) != -1) {
+        nlPos = options.indexOf("\n", pos);
+        QString port = options.mid(pos + portString.size(),
+                nlPos - pos - portString.size());
+
+        bool ok = false;
+        short portValue = port.toShort(&ok);
+        if (ok) {
+            m_port = portValue;
+            LOG_ENTRY(MyLogger::INFO, "Port number provided: "<<m_port);
+        } else {
+            LOG_ENTRY(MyLogger::ERROR,
+                    "Value: "<<port<<" is not a valid IP port.");
+            return -1;
+        }
+    } else {
+        LOG_ENTRY(MyLogger::INFO,
+                "Port number has not been provided. Default: "<<m_port<<" used.");
+
+    }
+
+    return 0;
 }
 
 } //namespace Base
